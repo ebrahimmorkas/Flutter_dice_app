b7d3ce25cb5224bc8dbc8edc226cf684e2a10984
Ebrahim <morkasebrahim3@gmail.com>
2024-03-03 14:49:32 +0530

    Dice App created with random generation of numbers

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,38 @@
 import "package:flutter/material.dart";
 // import "package:dice_app/StyledText.dart";
 // import "package:dice_app/gradient_widget.dart";
+import "dart:math";
 void main() {
-  void rollDice() {
-  }
-  runApp(
-     MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container(
+        body: MyDicePage(),
+      ),
+    );
+  }
+}
+
+class MyDicePage extends StatefulWidget {
+  @override
+  _MyDicePageState createState() => _MyDicePageState();
+}
+
+
+class _MyDicePageState extends State<MyDicePage> {
+  var dice_number = Random().nextInt(6) + 1;
+  void rollDice() {
+    setState(() {
+      dice_number = Random().nextInt(6) + 1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
           width: double.infinity,
           height: double.infinity,
           decoration: BoxDecoration(
@@ -16,7 +41,7 @@ void main() {
           child:  Column(
               mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset('../images/dice-images/dice-1.png', width: 200, height: 200,),
+              Image.asset('../images/dice-images/dice-$dice_number.png', width: 200, height: 200,),
               TextButton(
                 onPressed: rollDice, 
                 child: Text(
@@ -33,9 +58,6 @@ void main() {
               )
             ]
             ),
-          
-        )
-        ),
-      ),
-  );
+        );
+  }
 }
